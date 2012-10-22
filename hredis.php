6d435c03f58c4f77f39aed72f16fd92a4d9a6bcb
root <root@WPGATEWAY1.(none)>
2012-10-23 00:10:18 +0900

    change

--- hredis.php
+++ hredis.php
@@ -42,60 +42,29 @@ class NaiveDistributionStrategy implements IDistributionStrategy {
 
 class hRedis extends Predis\Client {
 
-	public $rServers = array(
-        array('host'=>'10.128.5.28','port'=>8000,'database'=>0,'alias'=>0),
-        array('host'=>'10.128.5.28','port'=>8001,'database'=>0,'alias'=>1),
-        array('host'=>'10.128.5.28','port'=>8002,'database'=>0,'alias'=>2),
-        array('host'=>'10.128.5.28','port'=>8003,'database'=>0,'alias'=>3),
-        array('host'=>'10.128.5.28','port'=>8004,'database'=>0,'alias'=>4),
-        array('host'=>'10.128.5.28','port'=>8005,'database'=>0,'alias'=>5),
-        array('host'=>'10.128.5.28','port'=>8006,'database'=>0,'alias'=>6),
+	public function __construct($sServerList, $databases = 0) {
 
-        array('host'=>'10.128.5.28','port'=>8007,'database'=>0,'alias'=>7),
-        array('host'=>'10.128.5.29','port'=>8008,'database'=>0,'alias'=>8),
-        array('host'=>'10.128.5.29','port'=>8009,'database'=>0,'alias'=>9),
-        array('host'=>'10.128.5.29','port'=>8010,'database'=>0,'alias'=>10),
-        array('host'=>'10.128.5.29','port'=>8011,'database'=>0,'alias'=>11),
-        array('host'=>'10.128.5.29','port'=>8012,'database'=>0,'alias'=>12),
-        array('host'=>'10.128.5.29','port'=>8013,'database'=>0,'alias'=>13),
-        array('host'=>'10.128.5.29','port'=>8014,'database'=>0,'alias'=>14),
-        array('host'=>'10.128.5.29','port'=>8015,'database'=>0,'alias'=>15),
+		$options = array(
+    		'cluster' => function() {
+	    		$distributor = new NaiveDistributionStrategy();
+	    		return new PredisCluster($distributor);
+	    	},
+		);
 
-        array('host'=>'10.128.5.30','port'=>8016,'database'=>0,'alias'=>16),
-        array('host'=>'10.128.5.30','port'=>8017,'database'=>0,'alias'=>17),
-        array('host'=>'10.128.5.30','port'=>8018,'database'=>0,'alias'=>18),
-        array('host'=>'10.128.5.30','port'=>8019,'database'=>0,'alias'=>19),
-        array('host'=>'10.128.5.30','port'=>8020,'database'=>0,'alias'=>20),
-        array('host'=>'10.128.5.30','port'=>8021,'database'=>0,'alias'=>21),
-        array('host'=>'10.128.5.30','port'=>8022,'database'=>0,'alias'=>22),
-        array('host'=>'10.128.5.30','port'=>8023,'database'=>0,'alias'=>23),
+		$nIndex			= 0;
+		$this->rServers	= array();
+		$lsServer		= explode(";", trim($sServerList,";"));	
+		foreach ( $lsServer as $k => $list ) {
 
-        array('host'=>'10.128.5.31','port'=>8024,'database'=>0,'alias'=>24),
-        array('host'=>'10.128.5.31','port'=>8025,'database'=>0,'alias'=>25),
-        array('host'=>'10.128.5.31','port'=>8026,'database'=>0,'alias'=>26),
-        array('host'=>'10.128.5.31','port'=>8027,'database'=>0,'alias'=>27),
-        array('host'=>'10.128.5.31','port'=>8028,'database'=>0,'alias'=>28),
-        array('host'=>'10.128.5.31','port'=>8029,'database'=>0,'alias'=>29),
-        array('host'=>'10.128.5.31','port'=>8030,'database'=>0,'alias'=>30),
-        array('host'=>'10.128.5.31','port'=>8031,'database'=>0,'alias'=>31),
-	);
-	public $options = null;
+			$temp		= explode(":", $list);
+			$host		= $temp[0];
+			$port		= $temp[1];
 
-	public function __construct($rServer = null, $options = null) {
+			$rSvrItem	= array('host'=>$host, 'port' => $port, 'database'=>$databases, 'alias'=> $nIndex);
+			array_push($this->rServers, $rSvrItem);
 
-		if ( empty($options) ) {
-			$this->options = array(
-    			'cluster' => function() {
-	    	    	$distributor = new NaiveDistributionStrategy();
-	    		    return new PredisCluster($distributor);
-	    		},
-			);
-		} else {
-			$this->options = $options;
-		}
+			$nIndex++;
 
-		if ( !empty($rServer) ) {
-			$this->rServers	= $rServer;
 		}
 
 		parent::__construct($this->rServers,$options);
